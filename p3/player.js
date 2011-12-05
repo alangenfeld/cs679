66cdefa7b008f4c2e615307fd09ceef69ad797d7
Dennis Paiz-Ramirez <dennispr@gmail.com>
2011-12-05 13:56:23 -0700

    Updated readability of the player class

--- p3/player.js
+++ p3/player.js
@@ -15,16 +15,22 @@ function Player(pos, dim, planeSize){
 
 		this.update = function(){	
     		var speed = 0.2;
+    		var roomEdge = planeSize/2 -.25;
     		//instead of making it continuous... make it snap?
     		//console.log("pos ="+this.pos[0]+" planelim =" +-planeSize/2);
-    		if(keyboard.left && this.pos[0]>-planeSize/2) {
+    		if(keyboard.left && this.pos[0]>-roomEdge) {
     		  this.pos[0] -= speed;
+<<<<<<< HEAD
 //    		  console.log("pos ="+this.pos[0]+" planelim =" +-planeSize/2);
     		} else if(keyboard.right && this.pos[0]<planeSize/2) {
+=======
+    		  //console.log("pos ="+this.pos[0]+" planelim =" +-planeSize/2);
+    		} else if(keyboard.right && this.pos[0]<roomEdge) {
+>>>>>>> Updated readability of the player class
     		  this.pos[0] += speed;
-    		} else if(keyboard.up && this.pos[1]<planeSize/2) {
+    		} else if(keyboard.up && this.pos[1]<roomEdge) {
     		  this.pos[1] += speed;
-    		} else if(keyboard.down && this.pos[1]>-planeSize/2) {
+    		} else if(keyboard.down && this.pos[1]>-roomEdge) {
     		  this.pos[1] -= speed;
     		}
     		var cooldown = 10;
@@ -37,8 +43,11 @@ function Player(pos, dim, planeSize){
       
     		mat4.multiplyVec3(mMatrix, this.pos, this.transformedPos);
     		mat4.multiplyVec3(mMatrix, this.light.pos, this.light.transformedPos);
-    		this.roomx = Math.round(this.pos[0]/(planeSize/roomSize)+2);;
-    		this.roomy = Math.round(this.pos[1]/(planeSize/roomSize)+2);;
+    		this.roomx = Math.round(this.pos[0]/(planeSize/roomSize)+2);
+    		this.roomy = Math.round(this.pos[1]/(planeSize/roomSize)+2);
+    		
+    		//am I at the edge? if so...
+    		
   		}
   		
 	}
